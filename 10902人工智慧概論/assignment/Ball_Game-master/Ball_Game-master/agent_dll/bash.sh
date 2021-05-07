1d26b5ce1d96cdee1d60eb6e89827cba48b390cc
anny-yanting <any20102010@gmail.com>
2021-05-07 16:57:10 +0800

    remove assignment

--- 10902人工智慧概論/assignment/Ball_Game-master/Ball_Game-master/agent_dll/bash.sh
+++
@@ -1,7 +0,0 @@
-g++.exe -Wall -DBUILD_DLL -g  -c C:\\Users\\MSCL\\Documents\\Ball_Game\\agent_dll\\agent_dll.cpp -o obj\\Debug\\agent_dll.o
-g++.exe -shared -Wl,--output-def=bin\\Debug\\libagent_dll.def -Wl,--out-implib=bin\\Debug\\libagent_dll.a -Wl,--dll  obj\\Debug\\agent_dll.o  -o bin\\Debug\\agent_dll.dll  -luser32
-
-cp ./bin/Debug/agent_dll.dll ./../2D_simple_ball_game
-	
-
-read -n 1 -p "Press any key to continue..."------------------------
